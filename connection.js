f6fafc939207c9f5e21d8611c09700040ffd7849
clarisas9003 <95923466+clarisas9003@users.noreply.github.com>
2022-10-08 16:42:04 -0400

    closer to connection

---
+++ connection.js
@@ -0,0 +1,46 @@
+
+    
+      
+    
+     
+      // TODO: Add SDKs for Firebase products that you want to use
+      // https://firebase.google.com/docs/web/setup#available-libraries
+  
+  
+
+  
+
+      function login(event){
+        event.preventDefault();
+        //sign in to DB
+        const auth = getAuth();
+        signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+        // Signed in 
+        const user = userCredential.user;
+        if(user){
+            alert('Welcome Back!')
+            debugger;
+        }
+      // ...
+        })
+        .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log('Wrong, try again', error.message)
+        debugger;
+        alert(error.message)
+        debugger;
+        });
+
+      }
+
+      function logout(){
+        const auth = getAuth();
+        signOut(auth).then(() => {
+          // Sign-out successful.
+        }).catch((error) => {
+          // An error happened.
+        });
+
+      }
