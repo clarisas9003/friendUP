585aad5d31fd87d501d7d1b40556119f27cf7170
clarisas9003 <95923466+clarisas9003@users.noreply.github.com>
2022-11-09 20:15:45 -0500

    Functioning

--- thumbsUpHandler.js
+++ thumbsUpHandler.js
@@ -6,11 +6,11 @@ var email = "";
 var uniqname = "";
   
 var count = 0;
-let description = [];
-let username = [];
-let FN = [];
-let LN = [];
-let major = [];
+let description = "";
+let username= "";
+let FN= "";
+let LN= "";
+let major= "";
 
 //document.getElementById("myHeader").textContent = username
 //var msgTitle = "<h1 class=\"myHeading\">Search Msg:" + username + "</h1>"
@@ -18,48 +18,18 @@ let major = [];
 
 
 function pageOnLoad(data){
-  
+ 
   description = data["Description"];
-  console.log(description);
   username = data["Username"];
-  console.log(username);
   FN = data["FN"];
   LN = data["LN"];
   major = data["Major"];
 
-  document.getElementById("userDisplayName").innerHTML = username[0];
-  document.getElementById("about").innerHTML = description[0];
+  document.getElementById("userDisplayName").innerHTML = username;
+  document.getElementById("about").innerHTML = description;
   
 }
-/*
-const db = getDatabase();
-const dbRef = ref(db, tableName);
-let list = [];
-onValue(dbRef, (snapshot) => {
-  console.log(snapshot);
-snapshot.forEach((childSnapshot) => {
-    //const childKey = childSnapshot.key;
-    const childData = childSnapshot.val();
-    list.push(childData);            
-    });
-}, {
-    onlyOnce: true
-});*/
 
-//console.log(list);
-/*const db = getDatabase();
-const rootRef = ref(db, tableName);
-const getTable = rRef(rootRef,"Aforger");
-onValue(getTable, (snapshot) => {
-  const data = snapshot.val();
-  console.log(data);*/
-  /*snapshot.forEach((AllRecords) => {
-    snapshot.forEach((CurrentRecord) => {
-        console.log(CurrentRecord.val().Description);
-      });
-    });*/
-  //});
-  /*firebase.database().ref(tableName).once('value',
 
 function yesFunction(){
   //yesArray.push(uniqname);
@@ -74,7 +44,7 @@ function yesFunction(){
   friendPending.style.display = "block";
   //pop current user into other user's suggestion
 */
-//}
+}
 
 function noFunction(){
   //load next element
